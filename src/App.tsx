e5360768c1c92513f143ddfb8e576be57a427211
Enrique Comesaña <enriquecbar@gmail.com>
2024-03-09 21:28:46 +0100

    video grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import { useState } from 'react';
-import CategoryPills from './components/CategoryPills';
-import { categories } from './data/home';
+
+import { CategoryPills } from './components/CategoryPills';
+import VideoGridItem from './components/VideoGridItem';
+import { categories, videos } from './data/home';
 import PageHeader from './pages/PageHeader';
+import Sidebar from './pages/Sidebar';
 
 export const App = () => {
   const [selectedCategory, setSelectedCategory] = useState(categories[0]);
@@ -9,7 +12,7 @@ export const App = () => {
     <div className='max-h-screen flex flex-col'>
       <PageHeader />
       <div className='grid grid-cols-[auto,1fr] flex-grow-1 overflow-auto'>
-        <div>Sidebar</div>
+        <Sidebar />
         <div className='overflow-x-hidden px-8 pb-4'>
           <div className='sticky top-0 bg-white z-10 pb-4 '>
             <CategoryPills
@@ -17,6 +20,11 @@ export const App = () => {
               selectedCategory={selectedCategory}
               onSelect={setSelectedCategory}
             />
+          </div>
+          <div className='grid gap-4 grid-cols-[repeat(auto-fill,minmax(300px,1fr))]'>
+            {videos.map(video => (
+              <VideoGridItem key={video.id} {...video} />
+            ))}
           </div>
         </div>
       </div>
